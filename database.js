53e5a137c282ba19d2d051e0873af7bd8be44f9e
ILikhacheva <ilikacheva@gmail.com>
2025-09-10 19:45:31 +0300

    server selection

--- database.js
+++ database.js
@@ -1,14 +1,19 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  /*host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,*/
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 5432,
+        database: process.env.DB_NAME || "horoscop",
+        user: process.env.DB_USER || "postgres",
+        password: process.env.DB_PASSWORD || "password",
+      }
+);
 
 (async () => {
   try {
